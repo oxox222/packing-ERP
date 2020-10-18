3cde94b2c46c2c0a2f1c92398d16d1e82e40d578
潘律彰 <741997717@qq.com>
2020-10-18 15:03:37 +0800

    出入库商品批量插入bug修复

--- src/main/java/com/plz/modules/controller/FileController.java
+++ src/main/java/com/plz/modules/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.plz.modules.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2020/9/18
  */
 @RestController
+@RequestMapping("/file")
 public class FileController {
 }
